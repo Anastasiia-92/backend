e07566cd25dbec6eca32546eda55bf72948bb2cf
AnastasiiaHusarenko <ahu@jooble.com>
2022-05-16 16:11:06 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json())
