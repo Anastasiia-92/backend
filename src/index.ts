0144c48cbae105631c5cbf34df47d4d651cdb3d0
AnastasiiaHusarenko <ahu@jooble.com>
2022-08-14 21:22:39 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,26 @@ let videos = [
     {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
 ]
 
+let bloggers = [
+    {id: 1, name: 'About JS - 01', youtubeUrl: 'it-incubator.eu'},
+    {id: 2, name: 'About JS - 02', youtubeUrl: 'it-incubator.eu'},
+    {id: 3, name: 'About JS - 03', youtubeUrl: 'it-incubator.eu'},
+    {id: 4, name: 'About JS - 04', youtubeUrl: 'it-incubator.eu'},
+    {id: 5, name: 'About JS - 05', youtubeUrl: 'it-incubator.eu'},
+]
+
+const showError = (field: string, message: string, res: Response) => {
+    const error = {
+        errorsMessages: [
+            {
+                message: message,
+                field: field,
+            }
+        ]
+    }
+    res.status(400).send(error)
+};
+
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send([
         {
@@ -83,7 +103,6 @@ app.put('/videos/:id', (req: Request, res: Response) => {
     }
     if (typeof validateBody === "string" && validateBody.length <= 40 && validateBody.length > 0) {
         video.title = req.body.title
-        //videos = videos.map((v) => v.id === +req.params.id ? {...v, title: req.body.title} : v)
         res.status(204).send(video)
     } else {
         const error = {
@@ -99,6 +118,115 @@ app.put('/videos/:id', (req: Request, res: Response) => {
     }
 
 })
+
+
+//bloggers
+
+app.get('/hs_01/api/bloggers', (req: Request, res: Response) => {
+    res.status(200).send(bloggers)
+})
+
+app.post('/hs_01/api/bloggers', (req: Request, res: Response) => {
+
+    const newBlogger = {
+        id: +(new Date()),
+        name: '',
+        youtubeUrl: '',
+    };
+
+    const name = req.body.name;
+    const youtubeUrl = req.body.youtubeUrl;
+
+    const regExp = "https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$";
+    const checkRegExp = new RegExp(regExp);
+
+    if (name?.trim().length) {
+        if (name.length > 15) {
+            return showError('name', 'maxLength: 15', res)
+        } else {
+            newBlogger.name = name
+        }
+    } else {
+        return showError('name', 'empty field', res)
+    }
+
+
+    if (youtubeUrl?.trim().length) {
+        if (youtubeUrl.length > 100) {
+            return showError('youtubeUrl', 'maxLength: 100', res)
+        } else {
+            newBlogger.youtubeUrl = youtubeUrl;
+        }
+        if (!youtubeUrl.match(checkRegExp)) {
+            return showError('youtubeUrl', 'invalid url', res)
+        }
+    } else {
+        return showError('youtubeUrl', 'empty field', res)
+    }
+
+    bloggers.push(newBlogger)
+    res.status(201).send(newBlogger)
+
+})
+
+app.get('/hs_01/api/bloggers/:bloggerId', (req: Request, res: Response) => {
+
+    const id = +req.params.bloggerId;
+    const blogger = bloggers.find(blogger => blogger.id === id)
+    !!blogger ? res.status(200).send(blogger) : res.status(404).send('blogger not found')
+})
+
+app.put('/hs_01/api/bloggers/:bloggerId', (req: Request, res: Response) => {
+
+    const name = req.body.name;
+    const youtubeUrl = req.body.youtubeUrl;
+
+    const regExp = "https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$";
+    const checkRegExp = new RegExp(regExp);
+
+    const id = +req.params.bloggerId;
+    const blogger = bloggers.find(blogger => blogger.id === id)
+
+    if (blogger) {
+        if (name?.trim().length) {
+            if (name.length > 15) {
+                return showError('name', 'maxLength: 15', res)
+            } else {
+                blogger.name = name
+            }
+        } else {
+            return showError('name', 'empty field', res)
+        }
+
+
+        if (youtubeUrl?.trim().length) {
+            if (youtubeUrl.length > 100) {
+                return showError('youtubeUrl', 'maxLength: 100', res)
+            } else {
+                blogger.youtubeUrl = youtubeUrl;
+            }
+            if (!youtubeUrl.match(checkRegExp)) {
+                return showError('youtubeUrl', 'invalid url', res)
+            }
+        } else {
+            return showError('youtubeUrl', 'empty field', res)
+        }
+        bloggers = [...bloggers, blogger]
+        res.status(204).send(bloggers)
+
+    } else {
+        res.status(404).send('blogger not found')
+    }
+
+})
+
+app.delete('/hs_01/api/bloggers/:bloggerId', (req: Request, res: Response) => {
+
+    const id = +req.params.bloggerId;
+    const blogger = bloggers.find(blogger => blogger.id === id)
+    !!blogger ? res.status(204) : res.status(404).send('blogger not found')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
